982e9d4d2158763a98d1d607f5dbe3677e77be3f
Ale <ale_pistilli@yahoo.com.ar>
2015-08-27 10:44:17 -0300

    Modifico script perl

--- prom-tom.pl
+++ prom-tom.pl
@@ -3,34 +3,31 @@
 use strict;
 use warnings;
 
-use LWP::UserAgent;
+use LWP::Simple;
 use HTTP::Request;
 
 my $filename = '/home/lale/pip-prom-tom/exa_prom.txt';
 my $fileout = '/home/lale/pip-prom-tom/exa_prom_out.fasta';
-my $ua = LWP::UserAgent->new;
-my $url;
-my $req;
-my $response;
-my $content;
 
-$ua->agent("Mozilla/4.0 (compatible; MSIE 5.0; Windows 98; DigExt)");
+my $content;
+my @tmpstr;
+
 open(my $fi, '<:encoding(UTF-8)', $filename) or die "Could not open file '$filename' $!";
 open(my $fo, ">$fileout") or die "No se puede abrir '$fileout' $!";
 
-# Buscar a cada uno de los promotores de heinz de solgeniomics los motivos
+# Buscar a cada uno de los promotores de heinz de solgeniomics los motivos 1000 upstream
 # que estan en la lista y descargar la cadena generando un fasta
 
-while (my $linea = <$fi>) {
+
+#while (my $linea = <$fi>) {
+	my $linea = "Solyc10g083560";
 	chomp $linea;
-	$url = "http://solgenomics.net/search/quick?term=$linea&x=51&y=8";
-	print "$url";
-	$req = HTTP::Request->new(GET => $url);
-	$response = $ua->request($req);
-	#$content = $response->content();
-	print $fo "$linea\n";
-	#print $content;
-	}
+	$content = get("http://solgenomics.net/search/quick?term=$linea&x=51&y=8");
+	print $linea;
+	@tmpstr = (split '<li><a class="xref xref_link xref_link_featurepages" href="/feature/">', $content);
+	#print $_, "\n" for split ' ', 'file1.gz file1.gz file3.gz';
+	print $fo "@tmpstr[1]\n";
+#	}
 
 close $fi;
 close $fo;
